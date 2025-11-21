75f16667ea695e76b4a81477169e876acb1f9583
Guadalupe <penaegeaguadalupe@gmail.com>
2025-11-21 17:37:59 +0100

    feat(toggle): Implement full color alternation logic (12/13 tests pass)

--- src/partTwo/js/events.js
+++ src/partTwo/js/events.js
@@ -1,3 +1,7 @@
 import { changeStyles } from "./partTwo.js";
 
 //Aquí codificaremos el evento que disparará el botón "Click me"
+
+const buttonElement = document.getElementById("click-me-button");
+
+buttonElement.addEventListener( "click" , changeStyles );
--- src/partTwo/js/partTwo.js
+++ src/partTwo/js/partTwo.js
@@ -1,15 +1,29 @@
 //II parte, refactorizar para hacer toggle, nos basaremos en lo que hicimos en el archivo partOne.js
 
-function changeColor(){
+
+function changeColor(newColor){
     //Escribe tu código aquí
+    const backgroundElement = document.getElementById("background");
+    backgroundElement.style.backgroundColor = newColor;
 };
 
-function changeText(){
+function changeText(newText){
     //Escribe tu código aquí
+    const textElement = document.getElementById("text");
+    textElement.textContent = newText;
 };
 
 function changeStyles(){
     //Escribe tu código aquí
+    const textElement = document.getElementById("text");
+
+    if (textElement.textContent === "red"){
+        changeColor("darkblue");
+        changeText("darkblue");
+    }else{
+        changeColor("red");
+        changeText("red");
+    }
 };
 
 export {changeColor, changeText, changeStyles}
